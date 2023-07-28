6647ccda9268088a6272a52fd1d3d319613f0882
Jose Torres <vidojam@gmail.com>
2023-07-28 15:55:42 -0400

    route work

--- src/app/Hello/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Hello () {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
-  return <div>Hello NextJS 13!</div>
-}
--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -8,23 +8,20 @@ export default function NavBar() {
   const pathname = usePathname();
 
   return (
-    <Navbar bg="primary" variant="dark" sticky="top" expand="sm" collapseOnSelect>
-       <Container>
+    <Navbar bg="info" variant="dark" color="black" sticky="top" expand="sm" collapseOnSelect>
+       <Container color="black">
           <Navbar.Brand as={Link} href="/">
                Portfolio
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="main-navbar" />
           <Navbar.Collapse id="main-navbar">
-            <Nav>
-              <Nav.Link as={Link} href="/Hello" active={ pathname === "/Hello" }>Hello</Nav.Link>
+            <Nav text-black >
+              <Nav.Link as={Link} href="/" active={ pathname === "/Projects" }>Home</Nav.Link>
               <Nav.Link as={Link} href="/Projects" active={ pathname === "/Projects" }>Projects</Nav.Link>
               <Nav.Link as={Link} href="/Certificates" active={ pathname === "/Certificates" }>Certificates</Nav.Link>
               <Nav.Link as={Link} href="/Resume" active={ pathname === "/Resume" }>Resume</Nav.Link>
               <Nav.Link as={Link} href="/Contact" active={ pathname === "/Contact" }>Contact</Nav.Link>
             </Nav>
-            {/* <Nav>
-              <Nav.Link as={Link} href="/projects">Projects</Nav.Link>
-            </Nav> */}
           </Navbar.Collapse>
        </Container>
     </Navbar>
